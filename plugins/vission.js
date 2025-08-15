a2556d8c04100bc71242349701068647a63ef89f
Humphrey47 <humphreymbise68@gmail.com>
2025-08-16 00:25:57 +0300

    Update vission.js

--- plugins/vission.js
+++ plugins/vission.js
@@ -53,12 +53,12 @@ cmd({
     const lastUpdate = fs.statSync(localVersionPath).mtime.toLocaleString();
 
     // GitHub stats
-    const githubRepo = 'https://github.com//humphreymbise/Queen_FAITH-MD-V-';
+    const githubRepo = 'https://github.com//humphreymbise/Queen_FAITH-MD-v4';
 
     // Check update status
-    let updateMessage = `✅ Your CRISS-AI bot is up-to-date!`;
+    let updateMessage = `✅ Your Queen_FAITH-MD-v4bot is up-to-date!`;
     if (localVersion !== latestVersion) {
-      updateMessage = `🚀 Your CRISS-AI bot is outdated!
+      updateMessage = `🚀 Your Queen_FAITH-MD-v4bot is outdated!
 🔹 *Current Version:* ${localVersion}
 🔹 *Latest Version:* ${latestVersion}
 
@@ -66,10 +66,10 @@ Use *.update* to update.`;
     }
 
     const statusMessage = `🌟 *Good ${new Date().getHours() < 12 ? 'Morning' : 'Night'}, ${pushname}!* 🌟\n\n` +
-      `📌 *Bot Name:* Queen_FAITH-MD-V5\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
+      `📌 *Bot Name:* Queen_FAITH-MD-v4\n🔖 *Current Version:* ${localVersion}\n📢 *Latest Version:* ${latestVersion}\n📂 *Total Plugins:* ${pluginCount}\n🔢 *Total Commands:* ${totalCommands}\n\n` +
       `💾 *System Info:*\n⏳ *Uptime:* ${uptime}\n📟 *RAM Usage:* ${ramUsage}MB / ${totalRam}MB\n⚙️ *Host Name:* ${hostName}\n📅 *Last Update:* ${lastUpdate}\n\n` +
       `📝 *Changelog:*\n${latestChangelog}\n\n` +
-      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [zezetech](https://github.com/humphreymbise/Queen_FAITH-MD-V-)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
+      `⭐ *GitHub Repo:* ${githubRepo}\n👤 *Owner:* [zezetech](https://github.com/humphreymbise/Queen_FAITH-MD-v4)\n\n${updateMessage}\n\n🚀 *Hey! Don't forget to fork & star the repo!*`;
 
     // Send the status message with an image
     await conn.sendMessage(from, {
@@ -80,8 +80,8 @@ Use *.update* to update.`;
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: 'Queen_FAITH-MD-V5',
-                        newsletterName: 'Queen_FAITH-MD-V5',
+                        newsletterJid: 'Queen_FAITH-MD-v4',
+                        newsletterName: 'Queen_FAITH-MD-v4',
                         serverMessageId: 143
         }
       }
